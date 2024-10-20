4cc2fd4e034ffd2cfdf31fc959b21df287b70bc9
Hecheng0625 <Hecheng0625@users.noreply.huggingface.co>
2024-10-20 06:39:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from transformers import SeamlessM4TFeatureExtractor
 
 processor = SeamlessM4TFeatureExtractor.from_pretrained("facebook/w2v-bert-2.0")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "CPU"
+device = torch.device("cuda" if torch.cuda.is_available() else "CPU")
 
 
 
