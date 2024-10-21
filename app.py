4a33f76b574f703bcbd151bf15190e192954cc4f
Hecheng0625 <Hecheng0625@users.noreply.huggingface.co>
2024-10-21 04:55:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -374,7 +374,7 @@ iface = gr.Interface(
     
     [![hf](https://img.shields.io/badge/%F0%9F%A4%97%20HuggingFace-demo-pink)](https://huggingface.co/spaces/amphion/maskgct)
     
-    [![readme](https://img.shields.io/badge/README-Key%20Features-blue)](../../../models/tts/maskgct/README.md)
+    [![readme](https://img.shields.io/badge/README-Key%20Features-blue)](https://github.com/open-mmlab/Amphion/tree/main/models/tts/maskgct)
     """
 )
 
