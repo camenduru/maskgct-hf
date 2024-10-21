f7a7aff3266cb7eeab40c8d4b1e6e598e12e415a
Hecheng0625 <Hecheng0625@users.noreply.huggingface.co>
2024-10-21 04:54:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import spaces
+import accelerate
 import gradio as gr
 import torch
 import safetensors
@@ -364,7 +365,17 @@ iface = gr.Interface(
     ],
     outputs=gr.Audio(label="Generated Audio"),
     title="MaskGCT TTS Demo",
-    description="Generate speech from text using the MaskGCT model.",
+    description="""
+    ## MaskGCT: Zero-Shot Text-to-Speech with Masked Generative Codec Transformer
+    
+    [![arXiv](https://img.shields.io/badge/arXiv-Paper-COLOR.svg)](https://arxiv.org/abs/2409.00750)
+    
+    [![hf](https://img.shields.io/badge/%F0%9F%A4%97%20HuggingFace-model-yellow)](https://huggingface.co/amphion/maskgct)
+    
+    [![hf](https://img.shields.io/badge/%F0%9F%A4%97%20HuggingFace-demo-pink)](https://huggingface.co/spaces/amphion/maskgct)
+    
+    [![readme](https://img.shields.io/badge/README-Key%20Features-blue)](../../../models/tts/maskgct/README.md)
+    """
 )
 
 # Launch the interface
